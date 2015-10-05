d3e8d7494fcd02f3c9bd8a3df3a12e6530046ba7
Damien Dosimont <damien.dosimont@imag.fr>
2015-10-05 17:58:38 +0200

    new script

---
+++ scripts/push_dev.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SERVER=mygrid.huma-num.fr
+USER=geomedia
+SSH=$USER@$SERVER
+WEBAPPS=/sites/geomedia/resource/tomcat/test/webapps
+REMOTE_DIST=$WEBAPPS/RSSAgregate.war
+REMOTE_WEB=$WEBAPPS/RSSAgregate
+
+echo "Pushing the application..."
+
+cd ..
+
+APP=`pwd`
+
+echo "Application local directory: $APP"
+
+LOCAL_DIST=$APP/dist/RSSAgregate.war
+LOCAL_WEB=$APP/build/web
+LOCAL_CONFIG=$APP/config/dev
+
+PERSISTENCE=$LOCAL_WEB/WEB-INF/classes/META-INF/persistence.xml
+LOG4J=$LOCAL_WEB/META-INF/context.xml
+CONTEXT=$LOCAL_WEB/WEB-INF/classes/log4j.properties
+
+echo "Installing configuration files..."
+
+cp $LOCAL_CONFIG/persistence.xml $PERSISTENCE
+cp $LOCAL_CONFIG/log4j.properties $LOG4J
+cp $LOCAL_CONFIG/context.xml $CONTEXT
+
+echo "Installing configuration files... OK"
+
+echo "Pushing the application..."
+
+rsync -avzr $LOCAL_DIST $SSH:$REMOTE_DIST
+rsync -avzr $LOCAL_WEB $SSH:$REMOTE_WEB
+
+echo "Pushing the application... OK"
+
+
