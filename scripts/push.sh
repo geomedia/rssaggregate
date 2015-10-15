f22fec1dbbd42c6629105b96de66899db7ea6292
Damien Dosimont <damien.dosimont@imag.fr>
2015-10-15 14:59:36 +0200

    scripts

--- scripts/push.sh
+++ scripts/push.sh
@@ -8,12 +8,13 @@ echo "Application local directory: $APP"
 
 LOCAL_DIST=$APP/dist/RSSAgregate.war
 LOCAL_WEB=$APP/build/web
-LOCAL_CONFIG=$APP/config/dev
-CONFIG_FILE=$APP/config/dev/configuration
+LOCAL_CONFIG=$APP/config/$1
+CONFIG_FILE=$LOCAL_CONFIG/configuration
 
 PERSISTENCE=$LOCAL_WEB/WEB-INF/classes/META-INF/persistence.xml
 CONTEXT=$LOCAL_WEB/META-INF/context.xml
-LOG4J=$LOCAL_WEB/WEB-INF/classes/log4j.properties
+LOG4J_PROPERTIES=$LOCAL_WEB/WEB-INF/classes/log4j.properties
+LOG4J=$LOCAL_WEB/WEB-INF/classes/log4j.xml
 
 source $CONFIG_FILE
 SSH=$USER@$SERVER
@@ -21,7 +22,8 @@ SSH=$USER@$SERVER
 echo "Installing configuration files..."
 
 cp $LOCAL_CONFIG/persistence.xml $PERSISTENCE
-cp $LOCAL_CONFIG/log4j.properties $LOG4J
+cp $LOCAL_CONFIG/log4j.properties $LOG4J_PROPERTIES
+cp $LOCAL_CONFIG/log4j.xml $LOG4J
 cp $LOCAL_CONFIG/context.xml $CONTEXT
 
 echo "Installing configuration files... OK"
